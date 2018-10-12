70ee42a063666e621a78c060d6e2c0c0be0b1cf8
Cristian Agudelo <cristiank13k@gmail.com>
2018-10-12 11:23:47 -0500

    Cambio error

---
+++ Parcial2/Parcial2/Controllers/ClientesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Parcial2.Models;
+
+namespace Parcial2.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+        public ActionResult Lista()
+        {
+            var Clientes = new Clientes()
+            {   
+                ID = 1,
+                Nombre = "Cristian Camilo",
+                Apellido = "Agudelo Rivera",
+                Sueldo = 100000
+                //Crear View
+            };
+            return View();
+        }
+    }
+}
--- Parcial2/Parcial2/Models/Clientes.cs
+++ Parcial2/Parcial2/Models/Clientes.cs
@@ -5,11 +5,11 @@ using System.Web;
 
 namespace Parcial2.Models
 {
-    public class Cliente
+    public class Clientes
     {
         public int ID { get; set; }
         public String Nombre { get; set; }
         public String Apellido { get; set; }
-        public float sueldo { get; set; }
+        public float Sueldo { get; set; }
     }
 }
